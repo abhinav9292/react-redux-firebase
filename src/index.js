ceb634d63683c767fa1e7438f44abf09a3ddc099
Abhinav Reddy Mareddy <mareddyabhinavreddy@gmail.com>
2019-01-30 11:48:36 -0800

    added root reducer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
 
 const store = createStore(rootReducer);
 
